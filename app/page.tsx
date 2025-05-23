f2e58ab189eeb8f141dbc126854860a7d560306e
Dominikg77 <dominik.graf2001@gmail.com>
2025-05-23 13:47:51 +0200

    basic setup, theme and font and packages

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+const DemoButton = () => {
+  return (
+    <>
+      <Button>Button</Button>
+      <h1> <strong>
+        Mein Text mit Araboto
+        </strong> </h1>
+      <p>Test asdsa d</p>
+    </>
+  );
+};
+
+export default DemoButton;
